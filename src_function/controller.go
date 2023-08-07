a046d6b7156067bcd93bb7d62d96113fd23da075
jrudio <justinjrudio@gmail.com>
2023-08-06 20:53:04 -0700

    cloud function: added additional logging, response to the client

--- src_function/controller.go
+++ src_function/controller.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -10,19 +12,45 @@ import (
 	sqladmin "google.golang.org/api/sqladmin/v1beta4"
 )
 
+type instanceStatePayload struct {
+	InstanceName string `json:"instanceName"`
+	State        string `json:"state"`
+}
+
 func HandleInstanceState(w http.ResponseWriter, r *http.Request) {
 	log.Println("function start")
 
-	instanceName := os.Getenv("INSTANCE_NAME")
-	desiredInstanceState := os.Getenv("INSTANCE_DESIRED_STATE")
-	projectID := os.Getenv("PROJECT_ID")
-
-	if instanceName == "" {
-		log.Fatalln("INSTANCE_NAME env var is required")
+	if r.Method != "POST" {
+		log.Printf("received unsupported method: '%s'\nexiting...", r.Method)
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
 	}
 
-	if desiredInstanceState == "" {
-		log.Fatalln("INSTANCE_DESIRED_STATE env var is required")
+	body := json.NewDecoder(r.Body)
+
+	var requestPayload instanceStatePayload
+
+	if err := body.Decode(&requestPayload); err != nil {
+		log.Printf("failed to parse request body: %v\n", err)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(fmt.Sprintf("failed to parse request body: %v\n", err)))
+		return
+	}
+
+	projectID := os.Getenv("PROJECT_ID")
+
+	if requestPayload.InstanceName == "" {
+		log.Println("instanceName is required")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("instanceName is required"))
+		return
+	}
+
+	if requestPayload.State == "" {
+		log.Println("state is required")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("state is required"))
+		return
 	}
 
 	if projectID == "" {
@@ -32,6 +60,8 @@ func HandleInstanceState(w http.ResponseWriter, r *http.Request) {
 
 		if err != nil {
 			log.Printf("failed to fetch project id: %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(fmt.Sprintf("failed to fetch project id: %v\n", err)))
 			return
 		}
 	}
@@ -41,6 +71,8 @@ func HandleInstanceState(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		log.Printf("failed to create sql admin service: %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprintf("failed to create sql admin service: %v\n", err)))
 		return
 	}
 
@@ -48,34 +80,46 @@ func HandleInstanceState(w http.ResponseWriter, r *http.Request) {
 	// 'NEVER'  == turn it off
 	instanceState := "ALWAYS"
 
-	if desiredInstanceState == "off" {
+	if requestPayload.State == "off" {
 		instanceState = "NEVER"
 	}
 
-	instanceGet := sqlAdminService.Instances.Get(projectID, instanceName)
+	log.Println("fetching instance state...")
+
+	instanceGet := sqlAdminService.Instances.Get(projectID, requestPayload.InstanceName)
 
 	instance, err := instanceGet.Do()
 
 	if err != nil {
 		log.Printf("failed to get cloud sql instance: %v\n", err)
-
+		w.WriteHeader(http.StatusBadGateway)
+		w.Write([]byte(fmt.Sprintf("failed to get cloud sql instance: %v\n", err)))
 		return
 	}
 
+	log.Println("fetched instance state")
+
 	instance.Settings.ActivationPolicy = instanceState
 
-	instancePatch := sqlAdminService.Instances.Patch(projectID, instanceName, instance)
+	instancePatch := sqlAdminService.Instances.Patch(projectID, requestPayload.InstanceName, instance)
+
+	log.Println("changing instance state...")
 
 	instancePatchResult, err := instancePatch.Do()
 
 	if err != nil {
 		log.Printf("failed to patch cloud sql instance: %v\n", err)
-
+		w.WriteHeader(http.StatusBadGateway)
+		w.Write([]byte(fmt.Sprintf("failed to patch cloud sql instance: %v\n", err)))
 		return
 	}
 
-	log.Printf("patch type: '%s', status: '%s'", instancePatchResult.OperationType, instancePatchResult.Status)
+	log.Println("successfully changed instance state")
 
-	// instancePatch
+	w.WriteHeader(http.StatusOK)
+
+	log.Printf("patch type: '%s', status: '%s'", instancePatchResult.OperationType, instancePatchResult.Status)
+	w.Write([]byte(fmt.Sprintf("patch type: '%s', status: '%s'", instancePatchResult.OperationType, instancePatchResult.Status)))
+
 	log.Println("function end")
 }
